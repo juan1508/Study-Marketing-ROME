eba70037c7ccc9974313166de646ed8e14b9557f
juan1508 <66849888+juan1508@users.noreply.github.com>
2026-03-18 15:44:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 st.set_page_config(
     page_title="Estudio de Mercado ROME",
-    page_icon="🔵",
+    page_icon="⭐",
     layout="wide",
     initial_sidebar_state="collapsed"
 )
